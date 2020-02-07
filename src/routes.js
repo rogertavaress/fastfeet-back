ddaea9270c66d8869c66002e2a589acf32c9cbd7
Rogério Tavares <rogerioctf@gmail.com>
2020-02-06 22:00:47 -0300

    Ajustando SessionController

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Funcionando...' }));
+routes.post('/Users', UserController.store);
+routes.put('/Users', authMiddleware, UserController.update);
+
+routes.post('/Session', SessionController.store);
 
 export default routes;
